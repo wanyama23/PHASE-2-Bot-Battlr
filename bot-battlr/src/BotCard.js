758527dbe558e7108e545183be1949e803a436f6
edmond <edmond.wanyama@student.moringaschool.com>
2023-07-28 23:40:57 +0300

    Botcard component rendered to react

---
+++ bot-battlr/src/BotCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const botTypeClasses = {
+    Assault: "icon military",
+    Defender: "icon shield",
+    Support: "icon plus circle",
+    Medic: "icon ambulance",
+    Witch: "icon magic",
+    Captain: "icon start",
+};
+
+function BotCard({bot, clickEvent, deleteBot}) {
+    return(
+        <div class="ui column">
+            
+        </div>
+    )
+}
